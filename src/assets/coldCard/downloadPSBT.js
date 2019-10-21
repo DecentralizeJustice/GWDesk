798e31354b61aba154ef06ee225d3b5a2444c55d
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-10-21 00:45:39 -0400

    Dialog windows small custom add

--- src/assets/coldCard/downloadPSBT.js
+++ src/assets/coldCard/downloadPSBT.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
-const remote = require('electron').remote
 const { dialog } = require('electron').remote
+const remote = require('electron').remote
 const app = remote.app
 
 async function downloadPSBT (psbtHex) {
--- src/assets/coldCard/uploadPSBT.js
+++ src/assets/coldCard/uploadPSBT.js
@@ -1,14 +1,15 @@
 const fs = require('fs-extra')
+const remote = require('electron').remote
+const app = remote.app
 const { dialog } = require('electron').remote
 
 async function uploadPSBT () {
-  // const options = {
-  //   title: 'Save file - Electron example',
-  //   defaultPath: app.getPath('desktop') + '/rerun.psbt',
-  //   buttonLabel: 'Save PSBT'
-  // }
+  const options = {
+    defaultPath: app.getPath('desktop'),
+    buttonLabel: 'Upload PSBT'
+  }
 
-  const fileName = await dialog.showOpenDialog(null)
+  const fileName = await dialog.showOpenDialog(null, options)
   const path = fileName.filePaths[0]
   const buffer = await fs.readFile(path)
   const hex = buffer.toString('hex')
