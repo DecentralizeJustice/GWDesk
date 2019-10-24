32f794f0f3668fbf45a069245bf67ea493855bb3
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-10-24 12:00:04 -0400

    Desktop sync app should not need any mnemonics

--- src/assets/constants/userConstantFiles.js
+++ src/assets/constants/userConstantFiles.js
@@ -1,7 +1,6 @@
-const webMnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost'
 const coldCardVpub = 'Vpub5nNANYCRfVmfgiGuMVtZQjcjGEfQZobjAYXpyjL1qr6TUpdgmrKRjWUgoFBsB4PNPqzomXsRxePQH23oqi2hYXMtm9zDfEsVQH5WfaQTLad'
 const webVpub = 'Vpub5nQxS4k2YAr2CeTKz7Kv5cTDLr8yEnEf4hV2S8xzmaK3ajsAeHAafma9Mtn6in9jykT5KBmDjqvVcjReza2gYpqvv2BHQb2eDgdWtBqqpej'
 const vpubObject = { coldcard: coldCardVpub, web: webVpub }
 const xfp = { coldcard: '6C6816CE', web: 'DFFED015' }
 
-export { webMnemonic, vpubObject, xfp }
+export { vpubObject, xfp }
