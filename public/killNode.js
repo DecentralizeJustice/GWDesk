a991d5e6cfb306609b527f72b458ee9c3ac6468f
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-10-27 20:03:31 -0400

    added strict

--- public/killNode.js
+++ public/killNode.js
@@ -2,7 +2,7 @@
 const find = require('find-process')
 
 async function killSPVNode () {
-  const list = await find('name', 'Distributed-Keys-bcoinSPVNode-2012')
+  const list = await find('name', 'Distributed-Keys-bcoinSPVNode-2012', true)
   const pid = list[0].pid
   process.kill(pid)
   process.exit()
