37ce51f9bfc738582476d7f17b56e9472c28f6ed
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-11-12 21:01:06 -0500

    Worked on detailed input info

--- src/components/transactions/mainCard.vue
+++ src/components/transactions/mainCard.vue
@@ -1,16 +1,20 @@
 <template>
   <v-container class="">
-      <v-row
-        v-for="item in transactions" :key="item.txid"
-        no-gutters
+    <v-expansion-panels
+        v-model="panel"
+        multiple
       >
-        <v-col>
-          <v-hover v-slot:default="{ hover }" >
+
+      <v-expansion-panel v-for="(item, index) in transactions" :key="item.txid"
+      class="mt-2 light-blue darken-4">
+
+      <v-expansion-panel-header>
+
           <v-card
             class='light-blue darken-4'
-            link
-            :elevation="hover ? 12 : 2"
+            :elevation="0"
           >
+
             <v-card-text class="mt-4 subtitle-1 white--text">
               <v-icon
               large v-bind:color="getColor(item.category)">
@@ -19,17 +23,70 @@
               {{item.amount}} BTC <v-divider/>
               {{getDate (item.blocktime)}}
             </v-card-text>
+
+            <v-expansion-panel-content>
+              <v-simple-table v-if="getType(item.category)==='sent'">
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Inputs</th>
+                      <th class="text-left">Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(stuff,index) in getInputInfo(item, index)" :key="index">
+                      <td>{{ stuff.address }}</td>
+                      <td>{{ stuff.value }}</td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+
+              <v-simple-table v-if="getType(item.category)!=='sent'">
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Input Addresses</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(stuff,index) in getInputInfo(item, index)" :key="index">
+                      <td v-if="stuff.address!==null">{{ stuff.address }}</td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+              <v-simple-table class="mt-3">
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Outputs</th>
+                      <th class="text-left">Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(stuff,index) in getOutputInfo(item, index)" :key="index">
+                      <td>{{ stuff.address }}</td>
+                      <td>{{ stuff.value }}</td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-expansion-panel-content>
           </v-card>
-        </v-hover>
-        </v-col>
-      </v-row>
-    </v-container>
+
+      </v-expansion-panel-header>
+        </v-expansion-panel>
+
+    </v-expansion-panels>
+  </v-container>
 </template>
 
 <script>
 export default {
   props: ['transactions'],
   data: () => ({
+    panel: []
   }),
   methods: {
     getType (type) {
@@ -73,6 +130,28 @@ export default {
       time += ' ' + monthNames[d.getMonth()]
       time += ' ' + hours + ':' + minutes + ' ' + ampm
       return time
+    },
+    getTotalValueInput (info, index) {
+      const type = info.category
+      if (type === 'send') {
+        const test = info.transactionInformation.inputs
+        return test
+      } else {
+        return info.transactionInformation.inputs
+      }
+    },
+    getInputInfo (info, index) {
+      const type = info.category
+      if (type === 'send') {
+        const test = info.transactionInformation.inputs
+        return test
+      } else {
+        return info.transactionInformation.inputs
+      }
+    },
+    getOutputInfo (info, index) {
+      const test = info.transactionInformation.outputs
+      return test
     }
   },
   async mounted () {
--- src/views/trans.vue
+++ src/views/trans.vue
@@ -25,7 +25,7 @@
 
 <script>
 import mainCard from '@/components/transactions/mainCard.vue'
-import { getWalletTransactions } from '@/assets/util/nodeUtil.js'
+import { getWalletTransactions, getTxByHash } from '@/assets/util/nodeUtil.js'
 import { account, walletName } from '@/assets/constants/genConstants.js'
 const R = require('ramda')
 export default {
@@ -37,10 +37,15 @@ export default {
   }),
   methods: {
   },
-  async mounted () {
+  async created () {
     const results = await getWalletTransactions(account, walletName)
     const sortByTime = R.sortBy(R.prop('blocktime'))
     const oldestLast = R.reverse(sortByTime(results))
+    for (var i = 0; i < oldestLast.length; i++) {
+      const txid = oldestLast[i].txid
+      const transInfo = await getTxByHash(txid, 'musig')
+      oldestLast[i].transactionInformation = transInfo
+    }
     this.transactions = oldestLast
   }
 }
