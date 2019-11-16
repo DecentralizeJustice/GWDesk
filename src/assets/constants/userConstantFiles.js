7ddf3423a8717ffffbe63636371062c5acbd3126
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-11-16 02:57:00 -0500

    added confirmed settings

--- src/assets/constants/userConstantFiles.js
+++ src/assets/constants/userConstantFiles.js
@@ -5,5 +5,6 @@ const xfp = { coldcard: '6C6816CE', web: 'DFFED015' }
 const token = '85ca804272de4e87fe6a9357260bc1b1b4eb5b7ffcba167eca17d60bc04e8e43'
 const apiKey = 'hunter2'
 const m = 2
+const neededConfirmedConfirmations = 2
 
-export { vpubObject, xfp, token, apiKey, m }
+export { vpubObject, xfp, token, apiKey, m, neededConfirmedConfirmations }
