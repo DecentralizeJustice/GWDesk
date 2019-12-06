0f1b7ad9a16af6cacaf8d4a4b5c4e970e7502ac9
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-12-06 00:19:22 -0500

    Removed gitlfs for mp4

--- notes/devNotes.md
+++ notes/devNotes.md
@@ -1,2 +1,1 @@
-0. Must install git lfs
 1. No windows 10 darkmode
--- src/components/video.vue
+++ src/components/video.vue
@@ -20,18 +20,12 @@ export default {
       hashPassed: false,
       bloburl: '',
       vidHash: '',
-      desiredHash: '2fd4d02c1247d10f22d8678f427d9ee787ce6093f4258d2eb4da8146980c6514c094338f001576ace76dc33c12623cb5afc3322e639757a1178973a2b5eb749e'
+      desiredHash: 'L9TQLBJH0Q8i2GePQn2e54fOYJP0JY0utNqBRpgMZRTAlDOPABV2rOdtwzwSYjy1r8MyLmOXV6EXiXOitet0ng=='
     }
   },
   methods: {
   },
   computed: {
-    // a computed getter
-    // hashOk: function () {
-    //   const passed = (this.desiredHash === this.vidHash)
-    //   console.log(this.vidHash)
-    //   return passed
-    // }
   },
   async beforeMount () {
     const url = this.url
@@ -45,28 +39,18 @@ export default {
     const urlb = await URL.createObjectURL(blob)
     this.bloburl = urlb
     var hash = crypto.createHash('sha512')
-    hash.setEncoding('hex')
     toBuffer(blob, async function (err, buffer) {
       if (err) throw err
       hash.update(buffer)
-      const yes = await hash.digest('hex')
-      if (this.desiredHash === yes) {
+      const testingHash = await hash.digest('base64')
+      console.log(testingHash)
+      if (this.desiredHash === testingHash) {
         this.hashPassed = true
         console.log('right hash')
       } else {
         console.log('wrong hash')
       }
     }.bind(this))
-
-    // hash.on('finish', function () {
-    //   const hashInfo = hash.read()
-    //   this.vidHash = hashInfo
-    //   this.hashPassed = true
-    //   console.log(this.desiredHash)
-    //   console.log(this.desiredHash === hashInfo)
-    //   this.test()
-    // })
-    // toStream(blob).pipe(hash)
   }
 }
 </script>
