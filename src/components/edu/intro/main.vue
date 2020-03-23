9564d2a12c23678bca302231e0c0d7a69e603ebb
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-03-23 00:25:44 -0400

    Major refactoring on edu course end

--- src/components/edu/intro/main.vue
+++ src/components/edu/intro/main.vue
@@ -1,21 +1,35 @@
 <template>
   <component
+  :key='lesson'
   v-bind:is="currentMain"
+  v-bind:courseInfo="courseInfo"
   v-on:exit="exit()"
   v-on:changeLesson="changeLesson"/>
 </template>
 <script>
+import whyCrypto from '@/assets/courseData/courses/intro/whyCrypto.js'
+import whichCrypto from '@/assets/courseData/courses/intro/whichCrypto.js'
+import landing from '@/components/edu/intro/landing.vue'
+import baseCourse from '@/components/edu/intro/baseCourse.vue'
 export default {
   data: () => ({
     lesson: ''
   }),
   computed: {
+    courseInfo () {
+      if (this.lesson === 'whichCrypto') {
+        return whichCrypto
+      }
+      if (this.lesson === 'whyCrypto') {
+        return whyCrypto
+      }
+      return true
+    },
     currentMain () {
       if (this.lesson === '') {
-        return () => import('@/components/edu/intro/landing.vue')
+        return landing
       }
-      const componentName = this.lesson
-      return () => import('@/components/edu/intro/' + componentName + '.vue')
+      return baseCourse
     }
   },
   methods: {
