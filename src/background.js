216c4ff7be54d9aca0799f4a0051381aa4c15632
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-06-16 17:28:37 -0400

    Download update and go

--- src/background.js
+++ src/background.js
@@ -12,11 +12,11 @@ const CHECK_FOR_UPDATE_PENDING = 'CHECK_FOR_UPDATE_PENDING'
 const CHECK_FOR_UPDATE_SUCCESS = 'CHECK_FOR_UPDATE_SUCCESS'
 const CHECK_FOR_UPDATE_FAILURE = 'CHECK_FOR_UPDATE_FAILURE'
 
-// const DOWNLOAD_UPDATE_PENDING = 'DOWNLOAD_UPDATE_PENDING'
-// const DOWNLOAD_UPDATE_SUCCESS = 'DOWNLOAD_UPDATE_SUCCESS'
-// const DOWNLOAD_UPDATE_FAILURE = 'DOWNLOAD_UPDATE_FAILURE'
+const DOWNLOAD_UPDATE_PENDING = 'DOWNLOAD_UPDATE_PENDING'
+const DOWNLOAD_UPDATE_SUCCESS = 'DOWNLOAD_UPDATE_SUCCESS'
+const DOWNLOAD_UPDATE_FAILURE = 'DOWNLOAD_UPDATE_FAILURE'
 
-// const QUIT_AND_INSTALL_UPDATE = 'QUIT_AND_INSTALL_UPDATE'
+const QUIT_AND_INSTALL_UPDATE = 'QUIT_AND_INSTALL_UPDATE'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -114,6 +114,24 @@ ipcMain.on(CHECK_FOR_UPDATE_PENDING, event => {
         sender.send(CHECK_FOR_UPDATE_FAILURE)
       })
   }
+})
+ipcMain.on(DOWNLOAD_UPDATE_PENDING, event => {
+  const result = autoUpdater.downloadUpdate()
+  const { sender } = event
+
+  result
+    .then(() => {
+      sender.send(DOWNLOAD_UPDATE_SUCCESS)
+    })
+    .catch(() => {
+      sender.send(DOWNLOAD_UPDATE_FAILURE)
+    })
+})
+ipcMain.on(QUIT_AND_INSTALL_UPDATE, () => {
+  autoUpdater.quitAndInstall(
+    true, // isSilent
+    true // isForceRunAfter, restart app after update is installed
+  )
 })
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
