5c7ede05e87071238920b952e5ef79667bff0bbf
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-06-16 23:00:33 -0400

    Refactored

--- src/background.js
+++ src/background.js
@@ -8,15 +8,6 @@ import {
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const { autoUpdater } = require('electron-updater')
 const { ipcMain } = require('electron')
-const CHECK_FOR_UPDATE_PENDING = 'CHECK_FOR_UPDATE_PENDING'
-const CHECK_FOR_UPDATE_SUCCESS = 'CHECK_FOR_UPDATE_SUCCESS'
-const CHECK_FOR_UPDATE_FAILURE = 'CHECK_FOR_UPDATE_FAILURE'
-
-const DOWNLOAD_UPDATE_PENDING = 'DOWNLOAD_UPDATE_PENDING'
-const DOWNLOAD_UPDATE_SUCCESS = 'DOWNLOAD_UPDATE_SUCCESS'
-const DOWNLOAD_UPDATE_FAILURE = 'DOWNLOAD_UPDATE_FAILURE'
-
-const QUIT_AND_INSTALL_UPDATE = 'QUIT_AND_INSTALL_UPDATE'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -96,7 +87,7 @@ app.on('ready', async () => {
   }
   createWindow()
 })
-ipcMain.on(CHECK_FOR_UPDATE_PENDING, event => {
+ipcMain.on('CHECK_FOR_UPDATE_PENDING', event => {
   const { sender } = event
 
   // Automatically invoke success on development environment.
@@ -108,32 +99,29 @@ ipcMain.on(CHECK_FOR_UPDATE_PENDING, event => {
     result
       .then((checkResult) => {
         const { updateInfo } = checkResult
-        sender.send(CHECK_FOR_UPDATE_SUCCESS, updateInfo)
+        sender.send('CHECK_FOR_UPDATE_SUCCESS', updateInfo)
       })
       .catch(() => {
-        sender.send(CHECK_FOR_UPDATE_FAILURE)
+        sender.send('CHECK_FOR_UPDATE_FAILURE')
       })
   }
 })
 
-ipcMain.on(DOWNLOAD_UPDATE_PENDING, event => {
+ipcMain.on('DOWNLOAD_UPDATE_PENDING', event => {
   const result = autoUpdater.downloadUpdate()
   const { sender } = event
 
   result
     .then(() => {
-      sender.send(DOWNLOAD_UPDATE_SUCCESS)
+      sender.send('DOWNLOAD_UPDATE_SUCCESS')
     })
     .catch(() => {
-      sender.send(DOWNLOAD_UPDATE_FAILURE)
+      sender.send('DOWNLOAD_UPDATE_FAILURE')
     })
 })
 
-ipcMain.on(QUIT_AND_INSTALL_UPDATE, () => {
-  autoUpdater.quitAndInstall(
-    true, // isSilent
-    true // isForceRunAfter, restart app after update is installed
-  )
+ipcMain.on('QUIT_AND_INSTALL_UPDATE', () => {
+  autoUpdater.quitAndInstall(true, true)
 })
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
