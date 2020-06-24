02b005052918868e6d0285a3f48d2ed88d196fda
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-06-23 23:37:15 -0400

    Removed new video

--- src/components/edu/intro/main.vue
+++ src/components/edu/intro/main.vue
@@ -20,7 +20,7 @@ export default {
       title: 'Intro To Cryptocurrency',
       lessons: [
         { title: 'Welcome to GW!', icon: 'cake', comp: 'welcome', unlocked: true },
-        { title: 'What is Cryptocurrency?', icon: 'atom-variant', comp: 'whyCrypto', unlocked: true },
+        { title: 'What is Cryptocurrency?', icon: 'atom-variant', comp: 'whyCrypto', unlocked: false },
         { title: 'Which Crypto is Best?', icon: 'account-question', comp: 'whichCrypto', unlocked: false },
         { title: 'How to store Crypto', icon: 'calculator', unlocked: false },
         { title: 'How To Buy Crypto', icon: 'account-cash', unlocked: false }
