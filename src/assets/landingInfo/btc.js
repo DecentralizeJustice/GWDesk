547f90e13d159bb54e498ddb084f58dc092cc7f1
unknown <32291941+itsMikeLowrey@users.noreply.github.com>
2020-09-01 15:54:02 -0400

    Added seperate component

--- src/assets/landingInfo/btc.js
+++ src/assets/landingInfo/btc.js
@@ -1,10 +1,10 @@
 import intro from '@/assets/courseData/courses/btc/intro.js'
-
+import exodusIntro from '@/assets/tutorialData/exodus/intro.js'
 const btcLandingInfo = {
   title: 'Bitcoin',
   lessons: [
     { title: 'Intro to Bitcoin', icon: 'cake', comp: intro, unlocked: true, first: true },
-    { title: 'Get Exodus Wallet', icon: 'atom-variant', unlocked: false },
+    { title: 'Exodus Wallet Setup', icon: 'atom-variant', comp: exodusIntro, unlocked: true },
     { title: 'Use Cash App', icon: 'account-question', unlocked: false }
   ]
 }
