c65f4919a36d27e5cc3352e3ef2f38d7160acc8f
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-09-28 01:21:07 -0400

    feat(components): sepeated lesson tabs into correct ones

---
+++ src/assets/landingInfo/coinbase.js
@@ -0,0 +1,11 @@
+
+import coinbaseSetup from '@/assets/lessons/tutorials/tutorials/coinbase/setup.js'
+import coinbaselinkbank from '@/assets/lessons/tutorials/tutorials/coinbase/linkAccount.js'
+const btcLandingInfo = {
+  title: 'Coinbase',
+  lessons: [
+    { title: 'Coinbase Setup', icon: 'account-question', comp: coinbaseSetup, unlocked: true },
+    { title: 'Coinbase Link Bank', icon: 'account-question', comp: coinbaselinkbank, unlocked: true }
+  ]
+}
+export default btcLandingInfo
