26ae6cbd5015da30756c8d493a783412c705131c
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-10-06 15:56:29 -0400

    feat: add deposit fiat lesson

--- src/assets/landingInfo/coinbase.js
+++ src/assets/landingInfo/coinbase.js
@@ -1,11 +1,13 @@
 
 import coinbaseSetup from '@/assets/lessons/tutorials/tutorials/coinbase/setup.js'
 import coinbaselinkbank from '@/assets/lessons/tutorials/tutorials/coinbase/linkAccount.js'
+import coinbaseDepositFiat from '@/assets/lessons/tutorials/tutorials/coinbase/depositFiat.js'
 const btcLandingInfo = {
   title: 'Coinbase',
   lessons: [
     { title: 'Coinbase Setup', icon: 'account-question', comp: coinbaseSetup, unlocked: true },
-    { title: 'Coinbase Link Bank', icon: 'account-question', comp: coinbaselinkbank, unlocked: true }
+    { title: 'Coinbase Link Bank', icon: 'account-question', comp: coinbaselinkbank, unlocked: true },
+    { title: 'Coinbase Deposit Fiat', icon: 'account-question', comp: coinbaseDepositFiat, unlocked: true }
   ]
 }
 export default btcLandingInfo
