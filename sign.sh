b484e373b0b553bf08356ea68dc8280f13fbb5cf
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-11-16 16:59:43 -0500

    run permissions

--- sign.sh
+++ sign.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-for i in 1 2 3 4 5
+## declare an array variable
+declare -a arr=("_sha1.cpython-37m-darwin.so")
+
+## now loop through the above array
+for i in "${arr[@]}"
 do
-  echo "Looping ... number $i"
+   codesign --deep --force --options runtime --verbose=4 --entitlements \"./buildInfo/pyentitlements.mac.inherit.plist\" --sign \"Developer ID Application: Decentralize Justice LLC\" --timestamp ./public/binaries/macHWI/$i
+   # or do whatever with individual element of the array
 done
