9e91bd3ea3859102dad91f49f12edc2c838aaa01
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-11-16 17:04:47 -0500

    no need to escape

--- sign.sh
+++ sign.sh
@@ -5,6 +5,6 @@ declare -a arr=("_sha1.cpython-37m-darwin.so")
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-   codesign --deep --force --options runtime --verbose=4 --entitlements \"./buildInfo/pyentitlements.mac.inherit.plist\" --sign \"Developer ID Application: Decentralize Justice LLC\" --timestamp ./public/binaries/macHWI/$i
+   codesign --deep --force --options runtime --verbose=4 --entitlements ./buildInfo/pyentitlements.mac.inherit.plist --sign "Developer ID Application: Decentralize Justice LLC" --timestamp ./public/binaries/macHWI/$i
    # or do whatever with individual element of the array
 done
