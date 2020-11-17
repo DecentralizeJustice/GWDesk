308f4fb0f806e9f122e51a989d01b5bdd530b38b
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-11-17 02:19:48 -0500

    tiny fix

--- electrumSign.sh
+++ electrumSign.sh
@@ -3,5 +3,5 @@
 arr=( $(find public/binaries/macElectrumGW -type f -name "*.so") )
 for ELEMENT in ${arr[@]}
 do
-codesign --force --options runtime --verbose=4 --entitlements ./buildInfo/pyentitlements.mac.inherit.plist --sign "Developer ID Application: Decentralize Justice LLC" --timestamp .$ELEMENT
+codesign --force --options runtime --verbose=4 --entitlements ./buildInfo/pyentitlements.mac.inherit.plist --sign "Developer ID Application: Decentralize Justice LLC" --timestamp ./$ELEMENT
 done
