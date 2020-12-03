250bbab89e2af71b53c7e6693a00d169bfcf30af
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2020-12-02 21:41:25 -0500

    lessons: why hw added why you need a hw wallet lessona and did some minor refactoring on the components for lessons

--- src/assets/landingInfo/wallet.js
+++ src/assets/landingInfo/wallet.js
@@ -1,9 +1,11 @@
 
 import intro from '@/assets/lessons/tutorials/tutorials/wallets/setup.js'
-const btcLandingInfo = {
+import whyHW from '@/assets/lessons/educational/lessons/wallet/whyHW.js'
+const info = {
   title: 'Wallet',
   lessons: [
-    { title: 'Testnet Wallet Guide', icon: 'cake', comp: intro, unlocked: true, first: true }
+    { title: 'Testnet Wallet Guide', icon: 'test-tube', comp: intro, unlocked: true, first: true },
+    { title: 'Why You Need A Hardware Wallet', icon: 'lock', comp: whyHW, unlocked: true, first: false }
   ]
 }
-export default btcLandingInfo
+export default info
---
+++ src/assets/lessons/educational/lessonData/eduTest/wallet/intro.js
@@ -0,0 +1,26 @@
+const questions = [
+  [
+    {
+      q: '_____ is responsible for sandboxing on most computer systems.',
+      opt: [
+        'Operating System',
+        'Kernel',
+        'Application Layer',
+        'Hardware'
+      ],
+      ans: 1
+    },
+    {
+      q: 'Most hardware wallets use a _____ Element to protect your private key from physical attacks.',
+      opt: [
+        'Morpheus',
+        'Hardened',
+        'Kernel',
+        'Secure'
+      ],
+      ans: 3
+    }
+  ]
+]
+
+export default questions
