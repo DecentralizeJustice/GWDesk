0b023380cab087758807cbbfd301736a9b5f7b5f
itsMikeLowrey <>
2020-12-29 00:22:18 -0500

    Remove lfs

--- public/binaries/windowsTrezorTool/attr/converters.pyi
+++ public/binaries/windowsTrezorTool/attr/converters.pyi
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7d51927dac05dcd332d84040a640bb740c0cbae8d60879c61279c082c6e45698
-size 380
+from typing import TypeVar, Optional, Callable, overload
+from . import _ConverterType
+
+_T = TypeVar("_T")
+
+def pipe(*validators: _ConverterType) -> _ConverterType: ...
+def optional(converter: _ConverterType) -> _ConverterType: ...
+@overload
+def default_if_none(default: _T) -> _ConverterType: ...
+@overload
+def default_if_none(*, factory: Callable[[], _T]) -> _ConverterType: ...
--- public/binaries/windowsTrezorTool/attr/filters.pyi
+++ public/binaries/windowsTrezorTool/attr/filters.pyi
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c43a6629094576cb20c466b9b6c97500387fdf3c00c004f8bd485df21fbcf939
-size 214
+from typing import Union, Any
+from . import Attribute, _FilterType
+
+def include(*what: Union[type, Attribute[Any]]) -> _FilterType[Any]: ...
+def exclude(*what: Union[type, Attribute[Any]]) -> _FilterType[Any]: ...
